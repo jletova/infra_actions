87d5a4a8632d8138f6416408bafd6170fa0d1863
Julia Letova <julialikh@gmail.com>
2022-04-14 16:14:11 +0200

    Проверка деплоя на сервер

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # infra_actions
 Учебный проект для изучения работы GitHub Actions (Яндекс Практикум)
+
+### 1. Создание образа:
+
+```bash
+docker build -t infra_action .
+```
+
+### 2. Запуск контейнера:
+
+```bash
+docker run --name infra_action -it -p 5001:5001 infra_action_cont
+```
--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -23,13 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k9gwcfq01xszx2(_4e#gc3u$rt=)y6ivx=$wt+5&-^ktk4dg4e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+DEBUG = False
+ALLOWED_HOSTS = '*'
 
 # Application definition
-
 INSTALLED_APPS = [
     'infra_app.apps.InfraAppConfig',
     'django.contrib.admin',
@@ -48,7 +45,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #
 ]
 
 ROOT_URLCONF = 'infra_project.urls'
